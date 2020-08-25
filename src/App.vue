2bfe67544d5b5bc1c7830a6e74090c264135a9a3
Sleeper <ntsleeper@gmail.com>
2020-08-25 13:06:15 -0600

    search and table setup

--- src/App.vue
+++ src/App.vue
@@ -1,94 +1,72 @@
 <template>
   <v-app id="inspire">
-    <v-navigation-drawer
-      v-model="drawer"
-      app
-    >
-      <v-list dense>
-        <v-list-item link>
-          <v-list-item-action>
-            <v-icon>mdi-home</v-icon>
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title>Home</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-        <v-list-item link>
-          <v-list-item-action>
-            <v-icon>mdi-email</v-icon>
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title>Contact</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-
-    <v-app-bar
-      app
-      color="indigo"
-      dark
-    >
-      <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
-      <v-toolbar-title>Application</v-toolbar-title>
+    <v-app-bar app color="indigo" dark>
+      <v-toolbar-title>Car-Gui</v-toolbar-title>
     </v-app-bar>
 
     <v-main>
-      <v-container
-        class="fill-height"
-        fluid
-      >
-        <v-row
-          align="center"
-          justify="center"
-        >
-
-
-        <template>
-  <v-data-table
-    :headers="headers"
-    :items="makes"
-    :items-per-page="12"
-    class="maketable"
-  ></v-data-table>
-</template>
-
-
-          <v-col class="text-center">
-            <v-tooltip left>
-              <template v-slot:activator="{ on }">
+      <template>
+        <v-container class="grey lighten-5">
+          <v-row no-gutters>
+            <v-col cols="6" md="4">
+              <v-card class="pa-2" outlined tile>
+                <v-combobox
+                  v-model="select"
+                  :items="items"
+                  label="Car Makes"
+                  multiple
+                ></v-combobox>
                 <v-btn
-                  :href="source"
-                  icon
-                  large
-                  target="_blank"
-                  v-on="on"
+                  v-on:click="requestCarInformation()"
+                  small
+                  color="primary"
+                  >Search</v-btn
                 >
-                  <v-icon large>mdi-code-tags</v-icon>
-                </v-btn>
-              </template>
-              <span>Source</span>
-            </v-tooltip>
-          </v-col>
-        </v-row>
-      </v-container>
+              </v-card>
+            </v-col>
+            <v-col cols="12" sm="6" md="8">
+              <v-card class="pa-2" outlined tile>
+                <template>
+                  <v-data-table
+                    :headers="car_table"
+                    :items="desserts"
+                    :items-per-page="5"
+                    class="elevation-1"
+                  ></v-data-table>
+                </template>
+              </v-card>
+            </v-col>
+          </v-row>
+        </v-container>
+      </template>
     </v-main>
-    <v-footer
-      color="indigo"
-      app
-    >
-      <span class="white--text">&copy; {{ new Date().getFullYear() }}</span>
+
+    <v-footer color="indigo" app>
+      <span class="white--text"
+        >&copy; {{ new Date().getFullYear() }} Spencer Thomas</span
+      >
     </v-footer>
   </v-app>
 </template>
 
 <script>
-  export default {
-    props: {
-      source: String,
+export default {
+  props: {
+    source: String,
+  },
+  data: () => ({
+    car_table: [
+      { text: "Makes", value: "Makes" },
+      { text: "Models", value: "Models" },
+      { text: "Value", value: "value" },
+    ],
+    test: false,
+  }),
+
+  methods: {
+    requestCarInformation: function() {
+      alert("hello");
     },
-    data: () => ({
-      drawer: null,
-    }),
-  }
+  },
+};
 </script>
